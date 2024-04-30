6b9ee2beedc92bac8214569f6cd0632757da5f21
KatyHummes <katielihummes15@gmail.com>
2024-04-30 14:26:49 -0300

    correção da auditoria

--- app/Models/DocAttach.php
+++ app/Models/DocAttach.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class DocAttach extends Model
+
+class DocAttach extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
 
     protected $fillable = [
         'protocol_id',
